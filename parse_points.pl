cdf7e8aeff65ac4319046685a0ef8bd1edb49696
wrosner <wrosner@kellerkind.rosner.lokal>
2026-01-08 17:43:09 +0100

    add coordinate translation

--- parse_points.pl
+++ parse_points.pl
@@ -8,6 +8,7 @@ use Data::Dumper qw(Dumper);
 use Geo::LibProj::cs2cs;
 use Geo::LibProj::FFI;
 
+#============= read by <> ================
 
 my @data;
 my $WKT;
@@ -25,17 +26,34 @@ while (<>) {
   # print "\n";
 }
 
+#========= process ===========
+
 my @labels = @{shift @data};
 
+# arrange data in hash by row# and column label
 my %d_hash;
-
 foreach my $ri (0 .. $#data) {
   my @dr = @{$data[$ri]};
   foreach my $ci (0 .. $#labels) {
     $d_hash{$ri}{$labels[$ci]}=$dr[$ci];
+    $d_hash{$ri}{IDX} = $ri;
   }
 }
 
+# ---------- try to create transfer machine ----------
+# my $cs2cs = Geo::LibProj::cs2cs->new("EPSG:25833" => "EPSG:4326");
+my $cs2cs = Geo::LibProj::cs2cs->new("ESRI:54043" => "WGS 84");
+
+foreach my $dhv (values %d_hash) {
+  my $point_in = [ $dhv->{mapX}, $dhv->{mapY} ];
+  my $point_out = $cs2cs->transform( $point_in  ) ;
+  $dhv->{lat} = $point_out->[0];
+  $dhv->{lon} = $point_out->[1];
+}
+
+
+
+#======= debug out ===========
 
 print "\n";
 print $WKT;
@@ -43,3 +61,7 @@ print "\n";
 
 print Data::Dumper->Dump([\@labels, \@data, \%d_hash], 
                         [qw(\@labels \@data \%d_hash)]);
+
+
+
+
