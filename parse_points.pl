472fb8a1d425742b5aa6101565b0b5e0b9eaf64c
wrosner <wrosner@kellerkind.rosner.lokal>
2026-01-08 21:09:18 +0100

    get statistics of lat/lon

--- parse_points.pl
+++ parse_points.pl
@@ -4,6 +4,7 @@ use warnings;
 use strict;
 
 use Data::Dumper qw(Dumper);
+use List::Util qw(min max sum);
 
 use Geo::LibProj::cs2cs;
 use Geo::LibProj::FFI;
@@ -69,6 +70,44 @@ print "\n";
 print Data::Dumper->Dump([\@labels, \@data, \%d_hash], 
                         [qw(\@labels \@data \%d_hash)]);
 
-# 
+# ---- prepare for estimators and defaults ---------
+# find extent of points in lat/lon space 
+
+# my ($lat_min, $lat_max, $lat_sum, $lon_min, $lon_max, $lon_sum);
+# my $point_cnt=0;
+# foreach my $dhv (values %d_hash) {
+#   $point_cnt++;
+#   my $plat = $dhv->{lat};
+#   my $plon = $dhv->{lon};
+#   $lat_sum += $plat;
+#   $lon_sum += $plon;
+# }
+# die "no points found" unless $point_cnt;
+# my $lat_avg = $lat_sum / $point_cnt;
+# my $lon_avg = $lon_sum / $point_cnt;
+
+my @lat_list = map { $_->{lat} } values %d_hash;
+my @lon_list = map { $_->{lon} } values %d_hash;
+
+die "no points processed" unless @lat_list && @lon_list;
+die "point number inconsistent" unless $#lat_list == $#lon_list; 
+
+my $lat_min = min @lat_list;
+my $lat_max = max @lat_list;
+my $lat_avg = (sum @lat_list) / (scalar @lat_list);
+
+my $lon_min = min @lon_list;
+my $lon_max = max @lon_list;
+my $lon_avg = (sum @lon_list) / (scalar @lon_list);
+
+
+print "\n";
+printf "extent ( \tmin \tavg \tmax \tnum) \n";
+printf "lat:    %f | %f | %f | %d \n",  $lat_min, $lat_avg, $lat_max , scalar @lat_list;
+printf "lon:    %f | %f | %f | %d \n",  $lon_min, $lon_avg, $lon_max , scalar @lon_list;
+print "\n";
+
+
+
 
 
